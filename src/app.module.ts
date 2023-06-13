dd4e5d35fc457d1a375e575016ea53f28abed322
usegen <hi@usegenerated.com>
2023-06-13 15:24:13 +0200

    initialization of use-generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+
+
 
 @Module({
-  imports: [],
+  imports: [  
+    GraphQLModule.forRoot({
+      debug: true,
+      introspection:true,
+      playground: true,
+      
+      autoSchemaFile: 'schema.gql',
+      context: ({ req }) => ({ req }),
+      driver: ApolloDriver
+  }),
+  MailerModule.forRoot({
+    transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+    defaults: {
+      from: '"nest-modules" <modules@nestjs.com>',
+    },
+    preview: true,
+    template: {
+      dir:`${process.cwd()}/mailtemplates`,
+      adapter: new HandlebarsAdapter(),
+      options: {
+        strict: true,
+      },
+    },
+  })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
